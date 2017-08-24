6cf62badd3673abead6d7e660be9deef95e1a290
hardikchoudhary <hardik.choudhary123@gmail.com>
2017-08-24 16:55:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,2 @@
-(function () {
-    "use strict";
-    exports.apiKey = process.env.GOOGLE_PLACES_API_KEY || "AIzaSyACurvXGgbQqG6fSe4-P1sOcEtwmyhXoTM";
-    exports.outputFormat = process.env.GOOGLE_PLACES_OUTPUT_FORMAT || "json";
-
-})();
-
+Migrated to dropbox
 
